aeb6d96aee905bb373773ba0bcfa97fa1c12760e
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-10 01:32:50 +0530

    Testing for valid First name

---
+++ src/main/java/com/bridgelabz/UserReg.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserReg {
+    Pattern pattern;
+    Matcher matcher;
+    public static final String validName = "^[A-Z][a-zA-Z]{2,}";
+
+    public  boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(validName);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+}
+
